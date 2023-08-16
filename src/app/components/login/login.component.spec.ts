78f19c204293b047a4c3402843e3495f50b3ddb5
Junior Almeida <98399474+Joot4@users.noreply.github.com>
2023-08-15 22:13:42 -0300

    Update login.component.spec.ts

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms'; // Importe o ReactiveFormsModule
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 
 
@@ -9,7 +9,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule], // Certifique-se de importar ReactiveFormsModule aqui
+      imports: [ReactiveFormsModule], 
       declarations: [LoginComponent]
     })
     .compileComponents();
